b887c6f94e774bbdd170189db4d783358bcc3f83
James-Relu <2975968973@qq.com>
2025-04-26 12:12:07 +0800

    1

--- Emitter.py
+++ Emitter.py
@@ -6,7 +6,7 @@ class Emitter(QObject):
 	# 定义信号
 	dynamic_signal = Signal(object)  # 可接收任意参数
 	page_change_signal = Signal(str)  # 向 main_stack发送改变页面的信号
-	create_event_signal = Signal(str, str, str, str, str, str, str)  # 标签、路径、年、月、日、主题、内容TODO：int or str?
+	create_event_signal = Signal(str, str, str, str, str, str, str)  # 标签、路径、年、月（两位）、日（两位）、主题、内容
 	search_signal = Signal(str)  # 发送sidebar搜索文本框的信息
 
 	def __init__(self, parent=None):
@@ -22,8 +22,8 @@ class Emitter(QObject):
 	def send_create_event_signal(self, name, filename, year, month, day, theme, content):
 		"""
 		发射可变数量的字符串参数
-		name为标签，如 create_ddl
-		格式：（标签，路径，日期，主题，内容）
+		name为标签，如 create_event
+		格式：标签、路径、年、月（两位）、日（两位）、主题、内容
 		"""
 		self.create_event_signal.emit(name, filename, year, month, day, theme, content)
 
