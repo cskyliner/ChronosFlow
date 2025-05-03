4b93a41c2445f69b4278b949a313bec196091f7b
James-Relu <2975968973@qq.com>
2025-05-03 22:57:53 +0800

    User b (#9)
    * 没有改动
    
    * 将代码格式化，增大间距，便于阅读
    
    * 去掉了Sidebar中的Schedul
    
    * Debug,将Emitter中与下文函数重名的信号send_search_somecolomns_event_signal改为search_some_columns_event_signal
    
    * 将MainWindow中控制Sidebar的按钮更名为sidebar_btn,将返回Calendar的按钮更名为close_btn,且各只保留了一个，而不是每个页面中各创建一个
    
    * 去掉了add_schedule;因为我使用了格式化，部分文件的空行会增加，可能造成冲突，但实际上功能无改变
    
    * Emitter中新增信号
    
    * 添加了Upcoming类
    
    * 1
    
    * 1

--- MainWindow.py
+++ MainWindow.py
@@ -8,9 +8,13 @@ from Settings import SettingsPage
 from Tray import Tray
 from FloatingWindow import FloatingWindow
 from Notice import Notice
+from Upcoming import Upcoming
+
 log = logging.getLogger(__name__)
+
+
 class MainWindow(QMainWindow):
-	
+
 	def __init__(self, width=800, height=600, show_x=100, show_y=100):
 		super().__init__()
 		# 尺寸
@@ -20,7 +24,7 @@ class MainWindow(QMainWindow):
 		self.setGeometry(show_x, show_y, width, height)
 
 		# 动画管理集
-		self.animations:dict[str,QPropertyAnimation] = {}
+		self.animations: dict[str, QPropertyAnimation] = {}
 
 		# 主窗口中心部件（容纳 main_layout）
 		self.central_widget = QWidget()
@@ -49,52 +53,70 @@ class MainWindow(QMainWindow):
 		# 通过名称记录页面，使用字典双向映射
 		self.main_stack_map = {}  # 名称→索引
 
+		# 按钮，控制侧边栏显示、隐藏
+		self.sidebar_btn = QPushButton("<")
+		self.sidebar_btn.setStyleSheet("""
+						            QPushButton {
+						                padding: 8px;
+						                background-color: transparent;
+						                border: 1px solid #ccc;
+						                border-radius: 4px;
+						            }
+						            QPushButton:hover {
+						                background-color: #e0e0e0;
+						            }
+						        """)
+		self.sidebar_btn.clicked.connect(self.toggle_sidebar)
+		# 返回按钮，回到calendar
+		self.return_btn = QPushButton("✕")
+		self.return_btn.setMinimumSize(20, 20)
+		self.return_btn.setStyleSheet("""
+								            QPushButton {
+								                padding: 8px;
+								                background-color: transparent;
+								                border: 1px solid #ccc;
+								                border-radius: 4px;
+								            }
+								            QPushButton:hover {
+								                background-color: #e0e0e0;
+								            }
+								        """)
+		self.return_btn.clicked.connect(partial(self.navigate_to, "Calendar", self.main_stack))
+
 		# 设置 main_stack各页面的内容
 		self.setup_main_window()  # 日历窗口（主界面）
 		self.setup_create_event_window()  # 日程填写窗口
+		self.setup_upcoming_window()  # 日程展示窗口
 		self.setup_setting_window()
-		self.setup_upcoming_window()
 
 		# 初始化通知系统
 		self.notice_system = Notice()
+		# 用于在通知时自动显示悬浮窗
+		self.notice_system.notify_show_floating_window.connect(self.show_floating_window_and_connect)
+		# 连接schedule_notice的信号
+		Emitter.instance().signal_to_schedule_notice.connect(self.notice_system.schedule_notice)
 
 		# 初始化托盘
 		self.tray = None
 		self._init_tray()
-        
-        # 悬浮窗口初始化
+
+		# 悬浮窗口初始化
 		self.floating_window = None
 
-		# 临时测试,手动添加了一个日程 (提供接口)
-		self.add_schedule()
-
-		# 或许可以有
-		# self.setWindowIcon(QIcon(self._get_icon_path()))
+	# 或许可以有
+	# self.setWindowIcon(QIcon(self._get_icon_path()))
 
 	def setup_main_window(self):
-		'''
+		"""
 		main_window创建
-		'''
+		"""
 		self.main_window = QWidget()
 		main_window_layout = QVBoxLayout()  # 内容区域布局
 		main_window_layout.setContentsMargins(20, 5, 20, 20)
 		self.main_window.setLayout(main_window_layout)
 
-		# 按钮，控制侧边栏显示、隐藏
-		self.toggle_btn = QPushButton("<")
-		self.toggle_btn.setStyleSheet("""
-		            QPushButton {
-		                padding: 8px;
-		                background-color: transparent;
-		                border: 1px solid #ccc;
-		                border-radius: 4px;
-		            }
-		            QPushButton:hover {
-		                background-color: #e0e0e0;
-		            }
-		        """)
-		self.toggle_btn.clicked.connect(self.toggle_sidebar)
-		main_window_layout.addWidget(self.toggle_btn, alignment=Qt.AlignmentFlag.AlignLeft)
+		# 添加'<'按钮
+		main_window_layout.addWidget(self.sidebar_btn, alignment=Qt.AlignmentFlag.AlignLeft)
 
 		# 创建日历界面
 		self.main_window_calendar = Calendar()
@@ -107,29 +129,19 @@ class MainWindow(QMainWindow):
 		self.add_page(self.main_stack, self.main_window, "Calendar")  # main_window 是日历，故名为calendar
 
 	def setup_create_event_window(self):
-		'''
+		"""
 		创建 create_event_window
-		'''
+		"""
 		self.create_event_window = QWidget()
 		schedule_layout = QVBoxLayout()  # 内容区域布局
 		schedule_layout.setContentsMargins(20, 5, 20, 20)
 		self.create_event_window.setLayout(schedule_layout)
 
-		# 返回按钮，回到calendar
-		self.schedule_toggle_btn = QPushButton("✕")
-		self.schedule_toggle_btn.setStyleSheet("""
-				            QPushButton {
-				                padding: 8px;
-				                background-color: transparent;
-				                border: 1px solid #ccc;
-				                border-radius: 4px;
-				            }
-				            QPushButton:hover {
-				                background-color: #e0e0e0;
-				            }
-				        """)
-		self.schedule_toggle_btn.clicked.connect(partial(self.navigate_to, "Calendar", self.main_stack))
-		schedule_layout.addWidget(self.schedule_toggle_btn, alignment=Qt.AlignmentFlag.AlignLeft)
+		# btn_layout.setContentsMargins(0, 0, 0, 0)  # 移除边距
+		# btn_layout.addWidget(self.sidebar_btn)  # 添加<按钮
+		# btn_layout.addStretch()
+		# TODO:不显示
+		schedule_layout.addWidget(self.return_btn, alignment=Qt.AlignmentFlag.AlignLeft)  # 添加✕按钮
 
 		# 创建Schedule
 		self.schedule = Schedule()
@@ -137,50 +149,41 @@ class MainWindow(QMainWindow):
 		self.add_page(self.main_stack, self.create_event_window, "Schedule")
 
 	def setup_setting_window(self):
-		'''
-		TODO:创建设置栏
-		'''
+		"""创建设置栏"""
 		self.setting_window = QWidget()
-		setting_layout = QVBoxLayout() # 内容区域布局
+		setting_layout = QVBoxLayout()  # 内容区域布局
 		setting_layout.setContentsMargins(20, 5, 20, 20)
 		self.setting_window.setLayout(setting_layout)
 
-		# 返回按钮，回到calendar
-		self.setting_toggle_btn = QPushButton("X")
-		self.setting_toggle_btn.setStyleSheet("""
-				            QPushButton {
-				                padding: 8px;
-				                background-color: transparent;
-				                border: 1px solid #ccc;
-				                border-radius: 4px;
-				            }
-				            QPushButton:hover {
-				                background-color: #e0e0e0;
-				            }
-				        """)
-		self.setting_toggle_btn.clicked.connect(partial(self.navigate_to, "Calendar", self.main_stack))
-		setting_layout.addWidget(self.setting_toggle_btn, alignment=Qt.AlignmentFlag.AlignLeft)
+		setting_layout.addWidget(self.return_btn, alignment=Qt.AlignmentFlag.AlignLeft)
 
 		self.setting = SettingsPage()
 		setting_layout.addWidget(self.setting)
 		self.add_page(self.main_stack, self.setting_window, "Setting")
 
-
-
 	def setup_upcoming_window(self):
-		'''
-		TODO:
-		'''
+		"""日程展示窗口"""
 		self.upcoming_window = QWidget()
+		layout = QVBoxLayout()  # 内容区域布局
+		layout.setContentsMargins(20, 5, 20, 20)
+		self.upcoming_window.setLayout(layout)
+
+		# TODO:不显示
+		layout.addWidget(self.return_btn, alignment=Qt.AlignmentFlag.AlignLeft)
+
+		# 连接信号TODO:具体讯息(哈希依据）
+
+		self.upcoming = Upcoming()
+		layout.addWidget(self.upcoming)
 		self.add_page(self.main_stack, self.upcoming_window, "Upcoming")
 
-	def add_page(self, stack:QStackedWidget, widget:QWidget, name:str):
+	def add_page(self, stack: QStackedWidget, widget: QWidget, name: str):
 		'''
 		向 stack 中添加页面
 		'''
 		self.main_stack_map[name] = stack.addWidget(widget)
 
-	def navigate_to(self, name:str, stack:QStackedWidget, date:QDate=None):
+	def navigate_to(self, name: str, stack: QStackedWidget, date: QDate = None):
 		'''
 		通过名称跳转页面
 		'''
@@ -212,16 +215,13 @@ class MainWindow(QMainWindow):
 		if self.sidebar_visible:
 			self.animations["sidebar"].setStartValue(0)
 			self.animations["sidebar"].setEndValue(230)
-			self.toggle_btn.setText("<")
+			self.sidebar_btn.setText("<")
 		else:
 			self.animations["sidebar"].setStartValue(230)
 			self.animations["sidebar"].setEndValue(0)
-			self.toggle_btn.setText(">")
+			self.sidebar_btn.setText(">")
 
 		self.animations["sidebar"].start()
-
-
-		
 
 	def _init_tray(self):
 		"""初始化系统托盘"""
@@ -237,8 +237,7 @@ class MainWindow(QMainWindow):
 		self.tray.show_notification("启动提醒", "程序已添加到系统托盘")
 		self.notice_system.notify_to_tray.connect(self.tray.notification_received)
 
-
-	def _get_icon_path(self):# 暂时无用
+	def _get_icon_path(self):  # 暂时无用
 		"""获取图标路径"""
 		base_dir = os.path.dirname(__file__)
 		return os.path.join(base_dir, "resources", "app_icon.ico")
@@ -261,19 +260,6 @@ class MainWindow(QMainWindow):
 		print("closeEvent 方法被调用")
 		event.accept()
 
-	def add_schedule(self, title = "会议提醒", content = "10分钟后有项目进度会议", 
-				  notify_time = QDateTime.currentDateTime().addSecs(5)
-				  , color="#3498db"):
-        # 示例：添加一个5秒后的通知
-		self.notice_system.schedule_notice(
-            title,
-           	content,
-            notify_time,
-            color # 绿色通知
-        )
-		# 用于在通知时自动显示悬浮窗
-		self.notice_system.notify_show_floating_window.connect(self.show_floating_window_and_connect)
-		
 	# 最小化按钮重定义为显示悬浮窗
 	def changeEvent(self, event):
 		# 检查事件类型是否为窗口状态改变事件
@@ -287,7 +273,7 @@ class MainWindow(QMainWindow):
 		# 调用父类的 changeEvent 方法以确保默认行为被执行
 		super().changeEvent(event)
 
-	#处理悬浮窗返回主窗口的逻辑
+	# 处理悬浮窗返回主窗口的逻辑
 	def show_main_window(self):
 		if self.windowState() == Qt.WindowMinimized:
 			print("目前最小化")
@@ -296,7 +282,7 @@ class MainWindow(QMainWindow):
 			print("目前隐藏")
 			self.show()
 
-	#打包操作
+	# 打包操作
 	def show_floating_window_and_connect(self):
 		# 显示悬浮窗
 		self.show_floating_window()
