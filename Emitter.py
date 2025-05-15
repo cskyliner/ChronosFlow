c9922c42cfe1177ca29e8d30b88f95c760430126
cskyliner <zkylin.cs@gmail.com>
2025-05-15 14:41:53 +0800

    User a feat: 实现了删除功能，新增全局ID表作为事件标识符，为后续多种事件作准备 (#25)
    * update frame settings
    
    * event start
    
    * my commit
    
    * userA: Event add/delete/modify/search function preliminary
    
    * my commit
    
    * start settings
    
    * 1
    
    * 1
    
    * 1
    
    * 1
    
    * 1
    
    * 1
    
    * 1
    
    * refactoring userB
    
    * refactoring userB
    
    * refactoring userB
    
    refactoring userB
    
    * 1
    
    * 1
    
    * 1
    
    * 1
    
    * Emitter changed
    
    * 1
    
    * 1
    
    * 1
    
    * userA: merge userB ,modify code style and add testdb_reset func
    
    * feat:修改了sidebar显示字体的bug，重写emitter避免多次实例化干扰连接，打包库在common中，连接前后端创建事件函数（仅支持DDL类）
    
    * Clean: squash all changes into one commit and create dev branch
    
    * feat:修改了全局搜索函数，支持多关键词模糊搜索；创建了新的信号；使用logging记录日志
    
    * feat:修改了全局搜索函数，支持多关键词模糊搜索；创建了新的信号；使用logging记录日志
    
    * fix:之前没有连接信号和后端函数，加入了SignalConnect管理信号连接
    
    * fix:解决了对于通知和托盘mac系统不兼容的问题，后端可以接收到前端的储存地址信息
    
    * fix: 修改了保存空日程警告框不弹出的问题
    
    * feat: 在Emitter中统一了向前端发送数据的回调函数，实现了upcoming对后端数据的读取，根据时间顺序排序，并实现懒加载
    
    * fix: 做了日历的适配
    
    * feat: 进行了后端对于通知信号的处理
    
    * feat: 实现了通知提醒前后端交互逻辑
    
    * feat: 实现了删除功能，新增全局ID表作为事件标识符，为后续多种事件作准备
    
    ---------
    
    Co-authored-by: James-Relu <2975968973@qq.com>

--- Emitter.py
+++ Emitter.py
@@ -11,19 +11,20 @@ class Emitter(QObject):
 	_instance = None  # 唯一实例
 	dynamic_signal: Signal = Signal(object)  								# 可接收任意参数
 	page_change_signal: Signal = Signal(str)  								# 向 main_stack发送改变页面的信号
+	signal_to_schedule_notice: Signal = Signal(str, str, QDateTime, str)  	# 向Notice中的schedule_notice函数发送信号
+	from_upcoming_to_create_event_signal: Signal = Signal(str, str)  		# 从upcoming跳转到create_event
+	refresh_upcoming_signal: Signal = Signal()								# 在切换到Upcoming时更新
 	create_event_signal: Signal = Signal(object)  							# 发送创建事件的信号
 	search_signal: Signal = Signal(str)  									# 发送sidebar搜索文本框的信息
 	storage_path_signal: Signal = Signal(object) 							# 发送存储路径的信号
-	update_upcoming_event_signal: Signal = Signal(object)  					# 向后端发送更新upcoming的信号
+	update_upcoming_event_signal: Signal = Signal(object)  					# 向后端发送更新upcoming的回调信号
 	delete_event_signal: Signal = Signal(object)  							# 发送删除事件的信号
 	search_all_event_signal: Signal = Signal(object)  						# 向后端发送搜索全局事件的信号
 	search_some_columns_event_signal: Signal = Signal(object)  				# 向后端发送搜索部分列事件的信号
 	search_time_event_signal: Signal = Signal(object)  						# 向后端发送搜索时间范围内事件的信号
 	backend_data_to_frontend_signal: Signal = Signal(object) 			 	# 向前端发送后端数据的信号
 	notice_signal: Signal = Signal(object)  								# 向通知栏发送最新数据
-	signal_to_schedule_notice: Signal = Signal(str, str, QDateTime, str)  	# 向Notice中的schedule_notice函数发送信号
-	from_upcoming_to_create_event_signal: Signal = Signal(str, str)  		# 从upcoming跳转到create_event
-	refresh_upcoming_signal: Signal = Signal()								# 在切换到Upcoming时更新
+	latest_event_signal_signal: Signal = Signal(object)						# 处理前端通知更新最新数据
 
 	@staticmethod
 	def instance() -> "Emitter":
@@ -90,17 +91,18 @@ class Emitter(QObject):
 		out = ("create_event", name, True, *args)
 		self.create_event_signal.emit(out)
 	
-	def send_delelte_event_signal(self, event_id: int,event_type: str):
+
+	def send_delelte_event_signal(self, event_id: int,event_table_type: str):
 		"""
 		发送删除事件的信号
 		event_id为事件ID
 		event_type为事件类型
 		"""
-		log.info(f"send delete event signal，事件ID为{event_id}，事件类型为{event_type}")
-		out = ("delete_event", event_id, event_type)
+		log.info(f"send delete event signal，事件ID为{event_id}，事件表名为{event_table_type}")
+		out = ("delete_event", (event_id, event_table_type))
 		self.delete_event_signal.emit(out)
 
-	# ===向后端发送请求（回传数据）===
+	# ===向后端发送请求（回传数据），回调信号===
 
 	def request_search_all_event_signal(self, keyword: tuple[str]):
 		"""
@@ -135,3 +137,12 @@ class Emitter(QObject):
 		log.info(f"request search some columns event，搜索列为{columns}，关键字为{keyword}")
 		out = ("search_some_columns", (columns, keyword))
 		self.search_some_columns_event_signal.emit(out)
+
+	def request_latest_event_signal(self, now_time: QDateTime):
+		"""
+		向后发送需要更新最新的事件
+		"""
+		formatted_time = now_time.toString("yyyy-MM-dd HH:mm")
+		log.info(f"request latest event，当前时间为{formatted_time}")
+		out = ("latest_event", formatted_time)
+		self.latest_event_signal_signal.emit(out)
