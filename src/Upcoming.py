2477ed50d6dd39a4b36a75d689192012e1b15d35
Uo <2726448705@qq.com>
2025-06-30 22:41:51 +0800

    增加热力图双击查看当日日程功能；优化当upcoming页面无任何ddl事件时的提示状态和双击查看某日日程时无任何日程提示显示(增加日期标签)

--- src/Upcoming.py
+++ src/Upcoming.py
@@ -368,6 +368,7 @@ class Upcoming(QListWidget):
 				log.info("正在加载数据，请稍等……")
 			elif self.no_more_events:
 				log.info("没有更多数据了，停止加载……")
+				self.notify_no_events()
 			else:
 				log.error("未知错误，无法加载数据")
 
@@ -647,7 +648,7 @@ class Upcoming(QListWidget):
 		self.loading = False
 		if self.no_more_events:
 			log.info("show_specific_date:没有更多数据了，停止加载……")
-			self.notify_no_events()
+			self.notify_no_events(date)
 			return
 		for event in self.events_used_to_update:
 			self.add_one_item(event)
@@ -671,16 +672,22 @@ class Upcoming(QListWidget):
 		self.load_more_data()
 		log.info(f"共{self.event_num}条日程")
 
-	def notify_no_events(self):
+	def notify_no_events(self, date: QDate = None):
 		# 创建自定义样式的提示项
 		# 创建提示项
+  
 		self.notify_item = QListWidgetItem()
 		self.notify_item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)  # 文字居中
-
 		# 使用Unicode符号+多行文本
-		notice_text = """📅 当前没有日程安排
-		────────────────
-		✨ 点击下方 + 号添加首个日程"""
+		if date is not None:
+			date_string = date.toString("yyyy年M月d日")
+			notice_text = f"""📅{date_string} 当日没有任何日程安排哦
+			────────────────
+			✨ 快来点击下方 + 号按钮添加首个日程吧"""
+		else:
+			notice_text = """📅 当前没有任何日程安排哦
+			────────────────
+			✨ 快来点击下方 + 号按钮添加首个日程吧"""
 
 		# 设置字体样式
 		set_font(self.notify_item, 4)
